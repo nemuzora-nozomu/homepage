527c3d0c03299df1ad5a66b8d60e8957a95637dd
Ihor Pavliuk <iktus2230@gmail.com>
2022-06-06 21:02:04 +0300

    Fixed old style and implemented easter trigger

--- script.js
+++ script.js
@@ -2,3 +2,15 @@
 document.querySelector('.darkmode-switch').addEventListener('click', () => {
   document.body.classList.toggle('dark');
 });
+
+// The (not) secret button
+const easterEggActivator = document.querySelector('.warning');
+const easterEggDeactivator = document.querySelector('.easter');
+const theme = document.querySelector('#site-design');
+
+easterEggActivator.addEventListener('click', () => {
+    theme.href = 'styles-easter.css';
+})
+easterEggDeactivator.addEventListener('click', () => {
+    theme.href = 'styles.css';
+})
